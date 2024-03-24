5b5c41356d942a0fc7f981cfbc9540136cae1ad5
ishan-surana <ishan5.mitmpl2022@learner.manipal.edu>
2024-03-25 02:31:00 +0530

    Updated streamlit_model.py

--- streamlit_model.py
+++ streamlit_model.py
@@ -17,6 +17,7 @@ import streamlit as st
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
 
 # Load data from CSV file
 data = pd.read_csv('reddit_posts_data.csv')
